4cd755cfc905a193f14bc7dcf7962de793d39a6e
Pache <etienne.raquin@insa-lyon.fr>
2019-10-30 10:28:23 +0100

    Fix Travis Attempt

--- tests.sh
+++ tests.sh
@@ -10,6 +10,12 @@ for test in $tests; do
     out="./tests/$test/out.txt"
     exp="./tests/$test/exp.txt"
     ./env.out < $in > $out
+    
+    echo "    output"
+    sed -n l $out
+    echo "    expected"
+    sed -n l $exp
+    
     if cmp -s "$exp" "$out"; then
         echo "  passed"
     else
